a2dc822d8499210347be6b6e6e42d4bd280cb020
Alexander Torino <alexander.torino@uconn.edu>
2025-06-25 17:25:45 -0400

    Class structure

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import tkinter as tk
 from tkinter import ttk
 
+class BookTrackerApp:
+    def __init__(self, root):
+        self.root = root
+        # set window title
+        self.root.title('Reading Tracker')
+
+         # set window size and location
+        self.root.geometry('600x400+50+50')
+
+        # load icon Windows
+        try:
+            self.root.iconbitmap('./assets/book.ico')
+        except tk.TclError:
+            print ("Windows icon file not found")
+        # load icon Linux/MacOS
+        try:
+            photo = tk.PhotoImage(file='./assets/book_icon.png')
+            self.root.iconphoto(False, photo)
+        except tk.TclError:
+            print("Linux/MacOS icon file not found")
+        
+        # Add Book button
+
+        # Frame for input Selection
+
 if __name__ == "__main__":
     root = tk.Tk()
-    # set window title
-    root.title('Reading Tracker')
-
-    # set window size and location
-    root.geometry('600x400+50+50')
-
-    # load icon Windows
-    try:
-        root.iconbitmap('./assets/book.ico')
-    except tk.TclError:
-        print ("Windows icon file not found")
-    # load icon Linux/MacOS
-    try:
-        photo = tk.PhotoImage(file='./assets/book_icon.png')
-        root.iconphoto(False, photo)
-    except tk.TclError:
-        print("Linux/MacOS icon file not found")
-    
-
-    # root window label
-    message = ttk.Label(root, text="Message")
-    # positions label on main window
-    message.pack()
-    
+    app = BookTrackerApp(root)
 
     # keep the window displaying for Windows macOS and Linux
     try:
