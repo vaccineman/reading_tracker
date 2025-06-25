0d6a2b768ac59fbd25dd82bf3c27874edcace786
Alexander Torino <alexander.torino@uconn.edu>
2025-06-25 19:05:11 -0400

    Added empty functions with docstrings outlining what they will be used for. Book data will be stored in a JSON file.

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ class BookTrackerApp:
          # set window size and location
         self.root.geometry('600x400+50+50')
 
+        # data files
+        self.data_file = 'books.json'
+        self.books = []
+
+        # load existing books
+        self._load_books() # attempt to load books on startup
+
         # load icon Windows
         try:
             self.root.iconbitmap('./assets/book.ico')
@@ -21,10 +28,54 @@ class BookTrackerApp:
             self.root.iconphoto(False, photo)
         except tk.TclError:
             print("Linux/MacOS icon file not found")
-        
-        # Add Book button
 
-        # Frame for input Selection
+        self._load_default_images() # pre-load book image placeholder
+        self._create_widgets() # build main app UI
+        self._refresh_book_display() # refresh the book list display
+
+        # bind window close function to save function
+        self.root.protocol("WM_DELETE_WINDOW", self._on_closing)
+
+    def _on_closing(self):
+        """
+        Handles window closing.
+        Saves current book data to file before destroying the window.
+        """
+        self._save_books()
+        self.root.destroy() # Close Tkinter application properly
+
+    def _load_books(self):
+        """
+        Loads book data from the JSON file.
+        If the file doesn't exist or is invalid, an empty list is initialized.
+        """
+        # TODO
+
+    def _save_books(self):
+        """
+        Saves current book data to the JSON file.
+        """
+        # TODO
+
+    def _load_default_images(self):
+        """
+        Loads a default placeholder image.
+        When a book doesn't have a selected image, or its file can't be found, a placeholder will be displayed.
+        """
+        # TODO
+    
+    def _create_widgets(self):
+        """
+        This sets up the main layout and widgets for the application.
+        """
+        # TODO
+
+    def _on_canvas_configure(self, event):
+        """
+        Callback function executed when the main canvas has been resized.
+        This updates the width of the inner book list frame to match the canvas width,
+        ensuring that book entries expand correctly and horizontal scrollbars are not needed.
+        """
 
 if __name__ == "__main__":
     root = tk.Tk()
